57e4fc11f0b3dde3e9c783e9b4baa91536efcb94
bukbunny <khomsan_su@cmu.ac.th>
2021-11-30 20:49:28 +0700

    complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,46 @@
 #include<iostream>
-
+#
+double adiff(double a,double b){
+  int c;
+  if(a<0){
+    while(a <= -360){
+       a += 360;
+    }
+    a = 360+a;
+  }else{
+    while(a>=360){
+      a -= 360;
+    }
+  }
+  if(b<0){
+    while(b <= -360){
+       b += 360;
+    }
+    b = 360+b;
+  }else{
+    while(b>=360){
+      b -= 360;
+    }
+  }
+  c = a-b;
+  if(c<0){
+    c *= -1;
+  }if(c >= 180){
+    c = 360-c;
+  }
+  return c;
+}
 using namespace std;
 
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  //  cout << adiff(730,270); //90
+  cout << adiff(180,270) << endl; //90
+  cout << adiff(210,45)<< endl; //165
+  cout << adiff(0,360)<< endl; //0
+  cout << adiff(10,350)<< endl; // 20
+  cout << adiff(95,260)<< endl; //165
+  cout << adiff(90,-90)<< endl; //180
+  cout << adiff(1000,280)<< endl; //0
+  cout << adiff(60,244)<< endl; //176
 }
